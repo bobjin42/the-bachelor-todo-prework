2793076803329da21d631a30e60378c08ed2689b
Yicheng Jin <bobjin42@gmail.com>
2018-06-21 15:57:16 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -35,7 +35,13 @@ def count_contestants_by_hometown(data, hometown)
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each{|season, ele|
+    ele.each{|info|
+    if info["hometown"] == hometown
+      return info["name"]
+    end
+    }
+  }
 end
 
 def get_average_age_for_season(data, season)
