e7614a9c953bcad245b7000ee09dbed5c963c7c5
Yicheng Jin <bobjin42@gmail.com>
2018-06-21 16:19:10 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,57 @@
 def get_first_name_of_season_winner(data, season)
   # code here
+  
+  data[season].each{|ele| 
+    if ele["status"] == "Winner"
+      name = ele["name"]
+      first_name = name.split(" ")[0]
+      return first_name
+    end
+  }
 end
 
 def get_contestant_name(data, occupation)
   # code here
+  data.each{|season, ele|
+    ele.each{|info|
+    if info["occupation"] == occupation
+      return info["name"]
+    end
+    }
+  }
 end
 
 def count_contestants_by_hometown(data, hometown)
   # code here
+  sum = 0
+  data.each{|season, ele|
+    ele.each{|info|
+    if info["hometown"] == hometown
+      sum += 1
+    end
+    }
+  }
+  return sum
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each{|season, ele|
+    ele.each{|info|
+    if info["hometown"] == hometown
+    return info["occupation"]
+    end
+    }
+  }
 end
 
 def get_average_age_for_season(data, season)
   # code here
+  people = 0
+  total_age = 0
+  data[season].each{|ele|
+    ages = ele["age"]
+    total_age += ages.to_f
+    people += 1
+  }
+  return (total_age / people).round
 end
