811ea6777f7eb377214768876a8b4d54c1455d75
Yicheng Jin <bobjin42@gmail.com>
2018-06-21 15:14:02 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,6 +1,10 @@
 def get_first_name_of_season_winner(data, season)
   # code here
-  data[season].each{|
+  
+  data[season].each{|ele| 
+    if ele[:status] == "Winner"
+      return ele[:name].split(" ")[0].join("")
+    end
 end
 
 def get_contestant_name(data, occupation)
